e331aabea2c4c1f313acbe95fe92b1c537c679f2
majinliang <18233698150@163.com>
2016-05-06 11:08:15 +0800

    5

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -3,68 +3,48 @@ public class MergeSortTest {
 
 	public static void main(String[] args) {
 		int[] data = new int[] { 5, 3, 6, 2, 1, 9, 4, 8, 7 };
-		mergeSort(data);
+		MergeSortTest m = new MergeSortTest();
+		m.sort(data, 0, data.length - 1);;
 		System.out.println("排序后的数组：");
 		print(data);
 	}
 
-	public static void mergeSort(int[] data) {
-		sort(data, 0, data.length - 1);
+	void sort(int[] data, int left, int right) {
+		if(left<right){
+			int mid = ( left + right)/2;
+			sort( data, left, mid);
+			sort( data, mid + 1, right);
+			merge(data, left, mid+1, right);
+		}
 	}
 
-	public static void sort(int[] data, int left, int right) {
-		if (left >= right)
-			return;
-		// 找出中间索引
-		int center = (left + right) / 2;
-		// 对左边数组进行递归
-		sort(data, left, center);
-		// 对右边数组进行递归
-		sort(data, center + 1, right);
-		// 合并
-		merge(data, left, center, right);
-	}
 
-	/**
-	 * 将两个数组进行归并，归并前面2个数组已有序，归并后依然有序
-	 * 
-	 * @param data
-	 *            数组对象
-	 * @param left
-	 *            左数组的第一个元素的索引
-	 * @param center
-	 *            左数组的最后一个元素的索引，center+1是右数组第一个元素的索引
-	 * @param right
-	 *            右数组最后一个元素的索引
-	 */
-	public static void merge(int[] data, int left, int center, int right) {
-		// 临时数组
-		int[] tmpArr = new int[data.length];
-		// 右数组第一个元素索引
-		int mid = center + 1;
-		// third 记录临时数组的索引
-		int third = left;
-		// 缓存左数组第一个元素的索引
-		int tmp = left;
-		while (left <= center && mid <= right) {
-			// 从两个数组中取出最小的放入临时数组
-			if (data[left] <= data[mid]) {
-				tmpArr[third++] = data[left++];
-			} else {
-				tmpArr[third++] = data[mid++];
+	void merge(int[] data, int left, int center, int right) {
+		int i;
+		int j = left;
+		int k = center;
+		int[] temp = new int[data.length];
+		for(i = left; (j<center)&&(k<right+1);i++){
+			if(data[j]<data[k]){
+				temp[i] = data[j];
+				j++;
+			}else{
+				temp[i] = data[k];
+				k++;
 			}
 		}
-		// 剩余部分依次放入临时数组（实际上两个while只会执行其中一个）
-		while (mid <= right) {
-			tmpArr[third++] = data[mid++];
+		while(j<center){
+			temp[i] = data[j];
+			j++;
+			i++;
 		}
-		while (left <= center) {
-			tmpArr[third++] = data[left++];
+		while(k<right+1){
+			temp[i] = data[k];
+			k++;
+			i++;
 		}
-		// 将临时数组中的内容拷贝回原数组中
-		// （原left-right范围的内容被复制回原数组）
-		while (tmp <= right) {
-			data[tmp] = tmpArr[tmp++];
+		for(int a = left; a<right+1; a++){
+			data[a] = temp[a];
 		}
 	}
 
