26ad212a6a361f9a0e086db04ba4729527779101
majinliang <18233698150@163.com>
2016-06-10 10:51:13 +0800

    insert sort

--- InsertionSort/src/insertionsort/Insertionsort.java
+++ InsertionSort/src/insertionsort/Insertionsort.java
@@ -1,7 +1,14 @@
 package insertionsort;
 /*
  * 插入排序
- * 算法思想：
+ * 插入排序有三种
+ * 1.直接插入排序
+ * 2.折半插入排序
+ * 3.希尔排序
+ *
+ * 这里实现的是直接插入排序
+ * 
+ * 直接插入排序算法思想：
  * 每次将一个待排序的记录
  * 按其关键字大小插入到前面已经排好序的子序列中的适当位置
  * 直到全部记录插入完成为止
@@ -10,16 +17,18 @@ public class Insertionsort {
 	
 	void sort(int[] x){
 		for(int i = 1; i < x.length; i++){
-			for(int a = i; a > 0; a--){
-				if(x[a]<x[a-1]){
-					int temp = x[a];
-					x[a] = x [a-1];
-					x[a -1] = temp;
+			if(x[i] < x[i - 1]){
+				int temp = x[i];
+				int a = i;
+				for(a = i; a > 0 && temp < x[a - 1]; a--){
+					x[a] = x[a -1];
 				}
+				x[a] = temp;
 			}
+				
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		int[] x = { 6, 2, 4, 1, 5, 9 };
 		Insertionsort b = new Insertionsort();
